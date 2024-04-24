e96c88723d37fa64f779616173dff6783fb77d71
Tobias <toob0102@gmail.com>
2024-04-25 00:24:27 +0200

    Untested!!!!! implementation of FreeRTOS VLC Sender

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,8 @@
 int led_pin = 11;
 long set_freq = 0;
 
-sen::VLCSender vlc_sender = { led_pin };
+sen::VLCSender vlc_sender = { led_pin, 1 };
 xTaskHandle vlc_sender_task_handle;
-
-void setup() {
-    Serial.begin( 9600 );
-    if ( Serial.available() ) {
-        Serial.println(
-            "What frequency would you like to initialize sending on?\n \
-            (this can be changed later)" );
-        set_freq = Serial.parseInt();
-    }
-
-    auto vlc_sender_task_return = xTaskCreate( vlc_sender_task, "VLC_SENDER_RUN", 2000,
-                                        NULL, 1, &vlc_sender_task_handle );
-    if ( vlc_sender_task_return != pdPASS ) {
-        vTaskDelete( vlc_sender_task_handle );
-        Serial.printf(
-            "== ERROR == Creating task for vlc_sender failed, error code = %d",
-            vlc_sender_task_return );
-    }
-}
 
 void vlc_sender_task( void* pvParameters ) {
     sen::VLCSender* vlc_sender = static_cast<sen::VLCSender*>( pvParameters );
@@ -36,5 +17,27 @@ void vlc_sender_task( void* pvParameters ) {
     delete vlc_sender;
 }
 
+void setup() {
+    Serial.begin( 9600 );
+    // if ( Serial.available() ) {
+    //     Serial.println(
+    //         "What frequency would you like to initialize sending on?\n \
+    //         (this can be changed later)" );
+    //     set_freq = Serial.parseInt();
+    // }
+
+    auto vlc_sender_task_return =
+        xTaskCreate( vlc_sender_task, "VLC_SENDER_RUN", 2000,
+                     (void*)&vlc_sender, 1, &vlc_sender_task_handle );
+    if ( vlc_sender_task_return != pdPASS ) {
+        vTaskDelete( vlc_sender_task_handle );
+        Serial.printf(
+            "== ERROR == Creating task for vlc_sender failed, error code = %d",
+            vlc_sender_task_return );
+    }
+}
+
 void loop() {
+    std::deque<uint8_t> bytes = { 0b11001100, 0b00110011 };
+    vlc_sender.sendBytes( bytes );
 }
