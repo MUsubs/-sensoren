c0c625bfbf2f5b9c9930ad2e2adbb2e426f62f29
Tobias <toob0102@gmail.com>
2024-05-15 14:12:20 +0200

    Demo version of VLC Sender with two instances multithreaded

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,38 @@
 
 #include "vlc_sender.hpp"
 
-int led_pin = LED_BUILTIN;
+int led_pin_1 = 20;
+int led_pin_2 = 19;
+
+int button_pin_1 = 7;
+int button_pin_2 = 3;
+
 long set_freq = 0;
 
-sen::VLCSender vlc_sender = { led_pin, 40 };
-xTaskHandle vlc_sender_task_handle;
+sen::VLCSender vlc_sender_1 = { led_pin_1, 60 };
+sen::VLCSender vlc_sender_2 = { led_pin_2, 120 };
+xTaskHandle vlc_sender_1_task_handle;
+xTaskHandle vlc_sender_2_task_handle;
 
-void vlc_sender_task( void* pvParameters ) {
+void vlc_sender_1_task( void* pvParameters ) {
     sen::VLCSender* vlc_sender = static_cast<sen::VLCSender*>( pvParameters );
 
     vlc_sender->run();
     Serial.printf(
         "== INFO == VLCSender task exited, deleting task and object" );
 
-    vTaskDelete( vlc_sender_task_handle );
+    vTaskDelete( vlc_sender_1_task_handle );
+    delete vlc_sender;
+}
+
+void vlc_sender_2_task( void* pvParameters ) {
+    sen::VLCSender* vlc_sender = static_cast<sen::VLCSender*>( pvParameters );
+
+    vlc_sender->run();
+    Serial.printf(
+        "== INFO == VLCSender task exited, deleting task and object" );
+
+    vTaskDelete( vlc_sender_2_task_handle );
     delete vlc_sender;
 }
 
@@ -25,30 +43,59 @@ void setup() {
     while ( !Serial ) {
         ;
     }
-    pinMode( 26, INPUT );
-    Serial.printf( "Creating VLC sender task\n" );
+    pinMode( led_pin_1, OUTPUT );
+    pinMode( led_pin_2, OUTPUT );
+    pinMode( button_pin_1, INPUT );
+    pinMode( button_pin_2, INPUT );
 
-    auto vlc_sender_task_return =
-        xTaskCreate( vlc_sender_task, "VLC_SENDER_RUN", 2000,
-                     (void*)&vlc_sender, 1, &vlc_sender_task_handle );
-    if ( vlc_sender_task_return != pdPASS ) {
-        vTaskDelete( vlc_sender_task_handle );
+    Serial.printf( "Creating VLC sender tasks\n" );
+
+    auto vlc_sender_1_task_return =
+        xTaskCreate( vlc_sender_1_task, "VLC_SENDER_RUN", 2000,
+                     (void*)&vlc_sender_1, 1, &vlc_sender_1_task_handle );
+    if ( vlc_sender_1_task_return != pdPASS ) {
+        vTaskDelete( vlc_sender_1_task_handle );
         Serial.printf(
-            "== ERROR == Creating task for vlc_sender failed, error code = "
+            "== ERROR == Creating task for vlc_sender_1 failed, error code = "
             "%d\n",
-            vlc_sender_task_return );
+            vlc_sender_1_task_return );
     }
-    Serial.printf( "== INFO == Created VLC Sender task in setup()\n" );
+    Serial.printf( "== INFO == Created VLC Sender 1 task in setup()\n" );
+
+    auto vlc_sender_2_task_return =
+        xTaskCreate( vlc_sender_2_task, "VLC_SENDER_RUN", 2000,
+                     (void*)&vlc_sender_2, 1, &vlc_sender_2_task_handle );
+    if ( vlc_sender_2_task_return != pdPASS ) {
+        vTaskDelete( vlc_sender_2_task_handle );
+        Serial.printf(
+            "== ERROR == Creating task for vlc_sender_2 failed, error code = "
+            "%d\n",
+            vlc_sender_2_task_return );
+    }
+    Serial.printf( "== INFO == Created VLC Sender 2 task in setup()\n" );
+    Serial.printf( "== INFO == All VLC_Sender tasks succesfully created\n" );
 }
 
 // 204, 51
 std::deque<uint8_t> bytes = { 0b11001100, 0b00110011 };
+bool button_pressed_1 = false;
+bool button_pressed_2 = false;
 
 void loop() {
-    if ( digitalRead( 26 ) == HIGH ) {
-        vlc_sender.sendBytes( bytes );
-        Serial.printf( "== MAIN == SENDING BYTES\n" );
-    } else {
+    if ( digitalRead( button_pin_1 ) && !button_pressed_1 ) {
+        Serial.printf("== COMMAND == Sending bytes VLC_Sender 1\n");
+        button_pressed_1 = true;
+        vlc_sender_1.sendBytes( bytes );
     }
-    vTaskDelay( 1000 );
+    if ( digitalRead( button_pin_2 ) && !button_pressed_2 ) {
+        Serial.printf("== COMMAND == Sending bytes VLC_Sender 2\n");
+        button_pressed_2 = true;
+        vlc_sender_2.sendBytes( bytes );
+    }
+    if ( !digitalRead( button_pin_1 ) ) {
+        button_pressed_1 = false;
+    }
+    if ( !digitalRead( button_pin_2 ) ) {
+        button_pressed_2 = false;
+    }
 }
--- src/vlc_sender.cpp
+++ src/vlc_sender.cpp
@@ -9,8 +9,8 @@ VLCSender::VLCSender( int led_pin, unsigned int frequency ) :
     pinMode( led_pin, OUTPUT );
     // bit delay in seconds
     if ( frequency <= 0 ) {
-        Serial.printf(
-            "== ERROR == frequency <= 0, setting it to 1 and continuing" );
+        // Serial.printf(
+            // "== ERROR == frequency <= 0, setting it to 1 and continuing" );
         frequency = 1;
     }
     bit_delay = 1.f / (float)frequency;
@@ -51,15 +51,15 @@ void VLCSender::run() {
             case state_t::IDLE:
                 if ( bytes_queue != NULL ) {
                     if ( xQueueReceive( bytes_queue, &bytes, 0 ) ) {
-                        Serial.printf( "Message received from queue\n" );
+                        // Serial.printf( "Message received from queue\n" );
                         index = 0;
                         byte_amount = bytes->size();
                         state = state_t::SENDSYNC;
-                        Serial.printf( "Frequency = %d | Bit Delay = %lf\n",
-                                       frequency, bit_delay );
+                        // Serial.printf( "Frequency = %d | Bit Delay = %lf\n",
+                                    //    frequency, bit_delay );
                     }
                 } else {
-                    Serial.printf( "== ERROR == Queue bytes_queue is NULL\n" );
+                    // Serial.printf( "== ERROR == Queue bytes_queue is NULL\n" );
                 }
                 break;
             // state 1
@@ -76,9 +76,9 @@ void VLCSender::run() {
                 break;
             // state 2
             case state_t::SENDBYTE:
-                Serial.printf( "Entering SENDBYTE\n" );
+                // Serial.printf( "Entering SENDBYTE\n" );
                 if ( index >= 8 ) {
-                    Serial.printf( "End of byte reached, Resetting index\n" );
+                    // Serial.printf( "End of byte reached, Resetting index\n" );
                     bytes->push_back( bytes->front() );
                     bytes->pop_front();
                     byte_amount--;
@@ -90,13 +90,13 @@ void VLCSender::run() {
                 }
                 return_state = state_t::SENDBYTE;
                 // send MSB first
-                Serial.printf( "%d items in deque\n", bytes->size() );
+                // Serial.printf( "%d items in deque\n", bytes->size() );
                 if ( bytes->front() )
-                    Serial.printf( "Current byte value : %d\n",
-                                   bytes->front() );
-                Serial.printf( "Index is %d\n", index );
+                    // Serial.printf( "Current byte value : %d\n",
+                                //    bytes->front() );
+                // Serial.printf( "Index is %d\n", index );
                 pin_state = ( bytes->front() & ( 0x80 >> index ) ) ? HIGH : LOW;
-                Serial.printf( "Pin state is %d\n", pin_state );
+                // Serial.printf( "Pin state is %d\n", pin_state );
                 index++;
                 state = state_t::SENDBIT;
                 break;
@@ -104,7 +104,7 @@ void VLCSender::run() {
             case state_t::SENDEND:
                 // set index to 42 for debugging purposes (this number shouldn't
                 // occur any other way)
-                Serial.printf("== INFO == All bytes succesfully sent\n");
+                // Serial.printf("== INFO == All bytes succesfully sent\n");
                 index = 42;
                 digitalWrite( led_pin, LOW );
                 return_state = state_t::IDLE;
