620d2c4bcdde82f42a7ff7a07410dad549b88dd2
Tobias <toob0102@gmail.com>
2024-06-05 17:11:12 +0200

    working sender without carrier freq, main setup for initial trying

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,20 @@
 unsigned int frequency = 4;
 int photo_pin = 26;
 
-int led_pin = LED_BUILTIN;
-long set_freq = 0;
+int led_pin = 3;
+unsigned int set_freq = 4;
 
-sen::VLCSender vlc_sender = { led_pin, 1 };
+sen::VLCSender vlc_sender = { led_pin, set_freq };
 xTaskHandle vlc_sender_task_handle;
 
-rec::Photodiode photodiode = { photo_pin, frequency };
-xTaskHandle photodiode_task_handle;
+// rec::Photodiode photodiode = { photo_pin, frequency };
+// xTaskHandle photodiode_task_handle;
 
-rec::VLCReceiver vlc_receiver = { photodiode, frequency };
-xTaskHandle vlc_receiver_task_handle;
+// rec::VLCReceiver vlc_receiver = { photodiode, frequency };
+// xTaskHandle vlc_receiver_task_handle;
 
-rec::VLCReceiverTest receiver_test = { vlc_receiver };
-xTaskHandle receiver_test_task_handle;
+// rec::VLCReceiverTest receiver_test = { vlc_receiver };
+// xTaskHandle receiver_test_task_handle;
 
 void vlc_sender_task( void* pvParameters ) {
     sen::VLCSender* vlc_sender = static_cast<sen::VLCSender*>( pvParameters );
@@ -34,85 +34,85 @@ void vlc_sender_task( void* pvParameters ) {
     delete vlc_sender;
 }
 
-void phototdiode_task( void* pvParameters ) {
-    rec::Photodiode* photodiode = static_cast<rec::Photodiode*>( pvParameters );
+// void phototdiode_task( void* pvParameters ) {
+//     rec::Photodiode* photodiode = static_cast<rec::Photodiode*>( pvParameters );
 
-    photodiode->run();
-    Serial.printf(
-        "== INFO == Photodiode task exited, deleting task and object" );
+//     photodiode->run();
+//     Serial.printf(
+//         "== INFO == Photodiode task exited, deleting task and object" );
 
-    vTaskDelete( photodiode_task_handle );
-    delete photodiode;
-}
+//     vTaskDelete( photodiode_task_handle );
+//     delete photodiode;
+// }
 
-void vlc_receiver_task( void* pvParameters ) {
-    rec::VLCReceiver* vlc_receiver =
-        static_cast<rec::VLCReceiver*>( pvParameters );
+// void vlc_receiver_task( void* pvParameters ) {
+//     rec::VLCReceiver* vlc_receiver =
+//         static_cast<rec::VLCReceiver*>( pvParameters );
 
-    vlc_receiver->run();
-    Serial.printf(
-        "== INFO == VLCReceiver task exited, deleting task and object" );
+//     vlc_receiver->run();
+//     Serial.printf(
+//         "== INFO == VLCReceiver task exited, deleting task and object" );
 
-    vTaskDelete( vlc_receiver_task_handle );
-    delete vlc_receiver;
-}
+//     vTaskDelete( vlc_receiver_task_handle );
+//     delete vlc_receiver;
+// }
 
-void vlc_receiver_test_task( void* pvParameters ) {
-    rec::VLCReceiverTest* receiver_test =
-        static_cast<rec::VLCReceiverTest*>( pvParameters );
+// void vlc_receiver_test_task( void* pvParameters ) {
+//     rec::VLCReceiverTest* receiver_test =
+//         static_cast<rec::VLCReceiverTest*>( pvParameters );
 
-    receiver_test->run();
-    Serial.printf(
-        "== INFO == VLCReceiverTest task exited, deleting task and object" );
+//     receiver_test->run();
+//     Serial.printf(
+//         "== INFO == VLCReceiverTest task exited, deleting task and object" );
 
-    vTaskDelete( receiver_test_task_handle );
-    delete receiver_test;
-}
+//     vTaskDelete( receiver_test_task_handle );
+//     delete receiver_test;
+// }
 
-bool CreatReceiverTasks() {
-    bool result = true;
+// bool CreatReceiverTasks() {
+//     bool result = true;
 
-    auto photodiode_task_return =
-        xTaskCreate( phototdiode_task, "PHOTODIODE_RUN", 2000,
-                     (void*)&photodiode, 1, &photodiode_task_handle );
-    if ( photodiode_task_return != pdPASS ) {
-        vTaskDelete( vlc_sender_task_handle );
-        Serial.printf(
-            "== ERROR == Creating task for photodiode failed, error code = "
-            "%d\n",
-            photodiode_task_return );
+//     auto photodiode_task_return =
+//         xTaskCreate( phototdiode_task, "PHOTODIODE_RUN", 2000,
+//                      (void*)&photodiode, 1, &photodiode_task_handle );
+//     if ( photodiode_task_return != pdPASS ) {
+//         vTaskDelete( vlc_sender_task_handle );
+//         Serial.printf(
+//             "== ERROR == Creating task for photodiode failed, error code = "
+//             "%d\n",
+//             photodiode_task_return );
 
-        result = false;
-    }
+//         result = false;
+//     }
 
-    auto vlc_receiver_task_return =
-        xTaskCreate( vlc_receiver_task, "VLC_RECEIVER_RUN", 2000,
-                     (void*)&vlc_receiver, 1, &vlc_receiver_task_handle );
-    if ( vlc_receiver_task_return != pdPASS ) {
-        vTaskDelete( vlc_receiver_task_handle );
-        Serial.printf(
-            "== ERROR == Creating task for vlc_receiver failed, error code = "
-            "%d\n",
-            vlc_receiver_task_return );
+//     auto vlc_receiver_task_return =
+//         xTaskCreate( vlc_receiver_task, "VLC_RECEIVER_RUN", 2000,
+//                      (void*)&vlc_receiver, 1, &vlc_receiver_task_handle );
+//     if ( vlc_receiver_task_return != pdPASS ) {
+//         vTaskDelete( vlc_receiver_task_handle );
+//         Serial.printf(
+//             "== ERROR == Creating task for vlc_receiver failed, error code = "
+//             "%d\n",
+//             vlc_receiver_task_return );
 
-        result = false;
-    }
+//         result = false;
+//     }
 
-    auto vlc_receiver_test_task_return =
-        xTaskCreate( vlc_receiver_test_task, "VLC_RECEIVER_TEST_RUN", 2000,
-                     (void*)&receiver_test, 1, &receiver_test_task_handle );
-    if ( vlc_receiver_test_task_return != pdPASS ) {
-        vTaskDelete( receiver_test_task_handle );
-        Serial.printf(
-            "== ERROR == Creating task for vlc_receiver failed, error code = "
-            "%d\n",
-            vlc_receiver_test_task_return );
+//     auto vlc_receiver_test_task_return =
+//         xTaskCreate( vlc_receiver_test_task, "VLC_RECEIVER_TEST_RUN", 2000,
+//                      (void*)&receiver_test, 1, &receiver_test_task_handle );
+//     if ( vlc_receiver_test_task_return != pdPASS ) {
+//         vTaskDelete( receiver_test_task_handle );
+//         Serial.printf(
+//             "== ERROR == Creating task for vlc_receiver failed, error code = "
+//             "%d\n",
+//             vlc_receiver_test_task_return );
 
-        result = false;
-    }
+//         result = false;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
 void setup() {
     Serial.begin( 19200 );
@@ -135,13 +135,13 @@ void setup() {
     // }
     // Serial.printf( "== INFO == Created VLC Sender task in setup()\n" );
 
-    bool result = CreatReceiverTasks();
+    // bool result = CreatReceiverTasks();
 
-    if ( result == false ) {
-        Serial.printf( "== INFO == Creating receiver tasks faild\n" );
-    } else {
-        Serial.printf( "== INFO == Created VLC Receiver tasks in setup()\n" );
-    }
+    // if ( result == false ) {
+    //     Serial.printf( "== INFO == Creating receiver tasks faild\n" );
+    // } else {
+    //     Serial.printf( "== INFO == Created VLC Receiver tasks in setup()\n" );
+    // }
 }
 
 // 204, 51
