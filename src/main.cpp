c5167cbd7a6c69bc4a6632d7cf203fd4f1bd0f88
Tobias <toob0102@gmail.com>
2024-06-11 13:21:50 +0200

    VLC Sender option for setting frequency through serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 
+#include <string>
+
 #include "vlc_sender.hpp"
 #include "vlc_sender_test.hpp"
 
 xTaskHandle vlc_sender_test_task_handle;
 xTaskHandle vlc_sender_task_handle;
 
-sen::VLCSender vlc_sender{3, 10};
+sen::VLCSender vlc_sender{ 3, 10 };
 
-void vlc_sender_test_task(void* pvParameters){
-    sen::VLCSenderTest vlc_sender_test(vlc_sender);
+void vlc_sender_test_task( void* pvParameters ) {
+    sen::VLCSenderTest vlc_sender_test( vlc_sender );
     vlc_sender_test.start();
 }
 
@@ -29,16 +31,17 @@ void setup() {
         ;
     }
     // wait for 2 seconds to let serial initialize
-    vTaskDelay(2000);
+    vTaskDelay( 2000 );
     Serial.printf( "== INIT == Creating VLC sender test task\n" );
 
     auto vlc_sender_test_task_return =
-        xTaskCreate( vlc_sender_test_task, "VLC_SENDER_TEST", 2000,
-                     NULL, 1, &vlc_sender_test_task_handle );
+        xTaskCreate( vlc_sender_test_task, "VLC_SENDER_TEST", 2000, NULL, 1,
+                     &vlc_sender_test_task_handle );
     if ( vlc_sender_test_task_return != pdPASS ) {
         vTaskDelete( vlc_sender_test_task_handle );
         Serial.printf(
-            "== ERROR == Creating task for vlc_sender_test failed, error code = "
+            "== ERROR == Creating task for vlc_sender_test failed, error code "
+            "= "
             "%d\n",
             vlc_sender_test_task_return );
     }
@@ -55,8 +58,14 @@ void setup() {
             vlc_sender_task_return );
     }
     Serial.printf( "== INFO == Created VLC Sender task in setup()\n" );
-
 }
 
 void loop() {
+    if ( Serial.available() > 0 ) {
+        String incoming_data = Serial.readString();
+        Serial.printf( "==INPUT== Setting VLC Sender Frequency to %d\n",
+                       incoming_data.toInt() );
+        vlc_sender.setFrequency( incoming_data.toInt() );
+    }
+    yield();
 }
